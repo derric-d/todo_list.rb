4b1fee8a967dc6a87107ad34f88d57faa5de422f
Derric Donehoo <dddonehoo7@gmail.com>
2019-02-06 12:07:52 +0100

    task func. added

--- todo.rb
+++ todo.rb
@@ -8,6 +8,7 @@ module Menu
 		4) Delete task
 		5) Write to a file 
 		6) Read from 
+		7) Toggle status
 		Q) Quit "
 	end
 	def show
@@ -51,25 +52,54 @@ class List
 	def show
 		all_tasks.map.with_index { |l, i| "(#{i.next}): #{l}"}
 	end
+
+	
 	def write_to_file(filename)
+		machinified = @all_tasks.map(&:to_machine).join("\n")
 		IO.write(filename, @all_tasks.map(&:to_s).join("\n"))
 	end
 	def read_from_file(filename)
 		IO.readlines(filename).each do |line|
+			status, *description = line.split(':')
+			status = status.include?('X')
 			add(Task.new(line.chomp))
 		end
+	end
+	def toggle(task_number)
+		all_tasks[task_number - 1].toggle_status
 	end
 end
 
 class Task
 	attr_reader :description
-	def initialize(description)
+	attr_accessor :status
+	def initialize(description, status = false)
 		@description = description
+		@status = status
 	end
 
 	def to_s
 		description
 	end
+
+	def completed?
+		status
+	end
+
+	def toggle_status
+		@completed_status = !completed?
+	end
+	def to_machine
+		"#{represent_status} : #{description}"
+	end
+
+	private
+
+	def represent_status
+		"#{completed? ? '[x]' : '[ ]'}"
+	end
+
+	
 end
 
 if __FILE__ == $PROGRAM_NAME 
@@ -97,6 +127,10 @@ if __FILE__ == $PROGRAM_NAME
     				my_list.write_to_file(prompt('What is the filename you want to write to? '))
     			when '6'
     				my_list.read_from_file(prompt('What file do you want to read from? '))
+    			when '7'
+		            puts my_list.show
+		            my_list.toggle(prompt('Which would you like to toggle the status for?').to_i)
+
     			
     			else
     				puts 'sorry, didnt understand'
